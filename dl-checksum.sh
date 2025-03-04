284beddb3ed6efea039630085ee12059f592f4a1
Andrew Rothstein <andrew.rothstein@gmail.com>
2025-03-04 17:17:50 +0000

    credhub:2.9.43

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -7,27 +7,29 @@ dl()
 {
     local ver=$1
     local os=$2
-    local archive_type=${3:-tgz}
-    local platform=${os}
+    local arch=$3
+    local archive_type=${4:-tgz}
+    local platform="${os}-${arch}"
     local file=credhub-${platform}-${ver}.${archive_type}
-    local url=$MIRROR/${ver}/$file
-    local lfile=$DIR/$file
+    local url="${MIRROR}/${ver}/${file}"
+    local lfile="${DIR}/${file}"
 
-    if [ ! -e $lfile ];
+    if [ ! -e "${lfile}" ];
     then
-        wget -q -O $lfile $url
+        curl -sSLf -o "$lfile" "$url"
     fi
 
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform `sha256sum $lfile | awk '{print $1}'`
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
 }
 
 dl_ver() {
     local ver=$1
     printf "  '%s':\n" $ver
-    dl $ver darwin
-    dl $ver linux
-    dl $ver windows
+    dl $ver darwin amd64
+    dl $ver darwin arm64
+    dl $ver linux amd64
+    dl $ver windows amd64
 }
 
-dl_ver ${1:-2.9.12}
+dl_ver ${1:-2.9.43}
